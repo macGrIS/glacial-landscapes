8baf4e6a24fd6656a63d872070fb53b16a794a0f
macGrIS <m.a.cooper@bristol.ac.uk>
2015-03-05 20:07:45 +0000

    updated script to read in and plot DEM and slope images -- need to now grid and compute metrics

--- landscapes.py
+++ landscapes.py
@@ -1,27 +1,99 @@
-# script by Michael A. Cooper (@macooperr) on 4th Mar 2015 : 18:12 GMT
+#-----------------------------------------------------------------------------------------
+#	4th March, 2015; 18:12 GMT
+#	Script by Michael A. Cooper (t: @macooperr; git: @macGrIS)
+#	to produce classification maps of 'landscapes of glacial erosion' (as per Jamieson, et
+#	al., 2014; Sugden and John, 1975) from GeoTIFF DEMs.
 #
-# produce classification of 'landscapes of glacial erosion' from DEMs
+#	Requirements:
+#	gdal
+#	numpy
+#	matplotlib
 #
+#-----------------------------------------------------------------------------------------
 
+### Header
+scriptname = 'Landscapes of Glacial Erosion -- Classifier'
+print 'Running script: ' + scriptname + '.'
+
+# Import python modules
 import gdal
 from gdalconst import *
-
 import numpy
+import matplotlib
+from matplotlib import pyplot as plt
+import os, sys, shutil
 
-DEM = '/Users/mc14909/Dropbox/Bristol/data/nuuk_mac/GIA_lip/GIA_bedDEM.tif'
-dataset = gdal.Open(DEM, GA_ReadOnly)
+### Define variables
+input_folder = '/Users/mc14909/Dropbox/Bristol/data/glacial-landscapes/'
+output_folder = '/Users/mc14909/Dropbox/Bristol/scratch/glacial-landscapes/'
+inputDEM = input_folder + 'GIA_bedDEM_clipped_nodata.tif'
+inputSlope = input_folder + 'GIA_bedDEM_slope.tif'
 
-cols = dataset.RasterXSize
-rows = dataset.RasterYSize
-driver = dataset.GetDriver().LongName
+grid_size = 100
+
+# Open DEM
+gDEM = gdal.Open(inputDEM, GA_ReadOnly)
+print 'DEM loaded sucessfully, checking file type...'
+
+# Check input DEM is GeoTIFF
+driver = gDEM.GetDriver().LongName
+if driver == 'GeoTIFF': # continues if GeoTIFF
+	print 'File type supported, continuing.'
+else:                   # aborts if not GeoTIFF
+	sys.exit('File type not supported, aborting.')
+
+# Read DEM into numpy array
+DEM = gDEM.ReadAsArray(0, 0, gDEM.RasterXSize, gDEM.RasterYSize).astype(numpy.float)
+print 'DEM sucessfully loaded into array.'
+
+# Check user for No Data?
+# DEM no data to NaN
+DEMnull = -9999
+a = numpy.where(DEM == DEMnull) # finds all values in array DEM equal to nodata_value
+DEM[a] = numpy.NaN # set nodata_value to NaN
+
+# Plot input files
+DEMplot = plt.imshow(DEM, cmap='bone')
+plt.savefig(output_folder + 'DEM_plot.png')
+
+# Open slope
+gSlope = gdal.Open(inputSlope, GA_ReadOnly)
+print 'Slope loaded sucessfully, checking file type...'
+
+# Check input slope is GeoTIFF
+driver = gSlope.GetDriver().LongName
+if driver == 'GeoTIFF': # continues if GeoTIFF
+    print 'File type supported, continuing.'
+else:                   # aborts if not GeoTIFF
+    sys.exit('File type not supported, aborting.')
+
+# Read slope into numpy array
+Slope = gSlope.ReadAsArray(0, 0, gSlope.RasterXSize, gSlope.RasterYSize).astype(numpy.float)
+print 'Slope sucessfully loaded into array.'
+
+# Slope no data to NaN
+Snull = Slope[0,0]
+b = numpy.where(Slope == Snull) # finds all values in array DEM equal to nodata_value
+Slope[b] = numpy.NaN # set nodata_value to NaN
+
+# Plot input files
+Splot = plt.imshow(Slope, cmap='coolwarm')
+plt.savefig(output_folder + 'Slope_plot.png')
+
+
+
+
+
+
+# Calculate Finite Slopes
+#X,Y = numpy.meshgrid(2501,3001)
+
+
+# Create fishnet/ subset grid
+
 
 # need to set up geolocation/ geotransform
 
-data = dataset.ReadasArray(0, 0, cols, rows).astype(numpy.float)
-
-# work out how to plot this array as surface plot
-# work out how to:
-# - create a 'slope' image
 # - create a fishnet of variable size (as in set the size as a variable -- look into fuzzy boxes later)
 # - produce a result of elevation range, and slope range (as per fishnet)
 # - produce a fishnet with smaller arrays for elevation values per fishnet box
