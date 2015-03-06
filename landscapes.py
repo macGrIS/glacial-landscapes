9df33c05ddc52f31e090cf0ec865144bbd3e1fb9
macGrIS <m.a.cooper@bristol.ac.uk>
2015-03-06 15:18:40 +0000

    further updates -- much to do

--- landscapes.py
+++ landscapes.py
@@ -3,12 +3,6 @@
 #	Script by Michael A. Cooper (t: @macooperr; git: @macGrIS)
 #	to produce classification maps of 'landscapes of glacial erosion' (as per Jamieson, et
 #	al., 2014; Sugden and John, 1975) from GeoTIFF DEMs.
-#
-#	Requirements:
-#	gdal
-#	numpy
-#	matplotlib
-#
 #-----------------------------------------------------------------------------------------
 
 ### Header
@@ -27,10 +21,11 @@ import os, sys, shutil
 input_folder = '/Users/mc14909/Dropbox/Bristol/data/glacial-landscapes/'
 output_folder = '/Users/mc14909/Dropbox/Bristol/scratch/glacial-landscapes/'
 inputDEM = input_folder + 'GIA_bedDEM_clipped_nodata.tif'
-inputSlope = input_folder + 'GIA_bedDEM_slope.tif'
+inputSlope = input_folder + 'GIA_bedDEM_slope.tif' # must be a way to calculate this
 
-grid_size = 100
+grid_size = 100 # size of fishnet
 
+### Call input DEM
 # Open DEM
 gDEM = gdal.Open(inputDEM, GA_ReadOnly)
 print 'DEM loaded sucessfully, checking file type...'
@@ -52,10 +47,9 @@ DEMnull = -9999
 a = numpy.where(DEM == DEMnull) # finds all values in array DEM equal to nodata_value
 DEM[a] = numpy.NaN # set nodata_value to NaN
 
-# Plot input files
-DEMplot = plt.imshow(DEM, cmap='bone')
-plt.savefig(output_folder + 'DEM_plot.png')
 
+### Calculate metrics
+## Calculate slope -- must be a way to calculate slope from DEM, for now open from .tiff created in Arc
 # Open slope
 gSlope = gdal.Open(inputSlope, GA_ReadOnly)
 print 'Slope loaded sucessfully, checking file type...'
@@ -68,24 +62,48 @@ else:                   # aborts if not GeoTIFF
     sys.exit('File type not supported, aborting.')
 
 # Read slope into numpy array
-Slope = gSlope.ReadAsArray(0, 0, gSlope.RasterXSize, gSlope.RasterYSize).astype(numpy.float)
+slope = gSlope.ReadAsArray(0, 0, gSlope.RasterXSize, gSlope.RasterYSize).astype(numpy.float)
 print 'Slope sucessfully loaded into array.'
 
 # Slope no data to NaN
-Snull = Slope[0,0]
-b = numpy.where(Slope == Snull) # finds all values in array DEM equal to nodata_value
-Slope[b] = numpy.NaN # set nodata_value to NaN
+sNull = slope[0,0]
+b = numpy.where(slope == sNull) # finds all values in array DEM equal to nodata_value
+slope[b] = numpy.NaN # set nodata_value to NaN
 
-# Plot input files
-Splot = plt.imshow(Slope, cmap='coolwarm')
+## Calculate peak analysis
+# Identify peaks
+"""
+    need to open DEM, and identify high points/ peaks within array of 1000, 1500, 2000, 2500 and 3000 metres with a minimum drop surrounding peak as 250 m
+http://nbviewer.ipython.org/github/demotu/BMC/blob/master/notebooks/DetectPeaks.ipynb
+
+"""
+
+# Peak density
+"""
+    density of peaks per fishnet
+"""
+
+## Calculate elevation range
+
+
+## Calculate slope range
+
+
+## Calculate hypsometry (elevation over area)
+
+### Plot outputs
+# Plot DEM
+DEMplot = plt.imshow(DEM, cmap='bone')
+plt.savefig(output_folder + 'DEM_plot.png')
+print 'DEM plotted successfully -- ' + output_folder + 'DEM_plot.png'
+
+# Plot slope
+Splot = plt.imshow(slope, cmap='bone')
 plt.savefig(output_folder + 'Slope_plot.png')
+print 'Slope plotted successfully -- ' + output_folder + 'Slope_plot.png'
 
 
 
-
-
-
-# Calculate Finite Slopes
 #X,Y = numpy.meshgrid(2501,3001)
 
 
