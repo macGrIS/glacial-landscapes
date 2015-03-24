4d19e97fa3bb6fcd06f372f086819808f4b64c84
macGrIS <m.a.cooper@bristol.ac.uk>
2015-03-24 14:36:35 +0000

    slope_notes, start of parameters

--- landscapes.py
+++ landscapes.py
@@ -17,13 +17,23 @@ import matplotlib
 from matplotlib import pyplot as plt
 import os, sys, shutil
 
-### Define variables
+### Define input
 input_folder = '/Users/mc14909/Dropbox/Bristol/data/glacial-landscapes/'
 output_folder = '/Users/mc14909/Dropbox/Bristol/scratch/glacial-landscapes/'
-inputDEM = input_folder + 'GIA_bedDEM_clipped_nodata.tif'
-inputSlope = input_folder + 'GIA_bedDEM_slope.tif' # must be a way to calculate this
+inputDEM = input_folder + 'GIA_bedDEM_clipped.tif'
+inputSlope = input_folder + 'GDAL_slope.tif' # must be a way to calculate this (see below), until then, using gdaldem slope output
 
-grid_size = 100 # size of fishnet
+### Define parameters and metrics
+# size of fishnet
+grid_size = 100
+# elevation range threshold lower
+
+# elevation range threshold upper
+
+# peak density threshold
+
+# slope threshold
+
 
 ### Call input DEM
 # Open DEM
@@ -41,15 +51,43 @@ else:                   # aborts if not GeoTIFF
 DEM = gDEM.ReadAsArray(0, 0, gDEM.RasterXSize, gDEM.RasterYSize).astype(numpy.float)
 print 'DEM sucessfully loaded into array.'
 
-# Check user for No Data?
+# Check user for No Data value?
 # DEM no data to NaN
-DEMnull = -9999
+DEMnull = DEM[0,0] # input no data value for tiff
 a = numpy.where(DEM == DEMnull) # finds all values in array DEM equal to nodata_value
 DEM[a] = numpy.NaN # set nodata_value to NaN
 
 
+
 ### Calculate metrics
-## Calculate slope -- must be a way to calculate slope from DEM, for now open from .tiff created in Arc
+
+## Calculate slope
+""" 
+Now I'm not a mathematician, so I'm not sure how this works -- but this method 
+of calculating slope is not correct (yeilds a roughly inverse, but still different
+result than that of gdaldem slope, and ArcGIS slope...)
+
+Until I work this out, we use GDAL output
+
+x, y = numpy.gradient(DEM, X) # calculate gradient with sample distance X (define)
+
+slope_test = numpy.pi/2. - numpy.arctan(numpy.sqrt(x*x + y*y))
+for i in numpy.nditer(slope_test, op_flags=['readwrite']):
+    i[...] = numpy.degrees(i)
+
+
+#rad2deg = 180.0 / math.pi
+#slope_test2 = 90.0 - arctan(sqrt(x*x + y*y)) * rad2deg
+#
+#c = numpy.where(numpy.isnan(slope_test))
+#slope_test[c] = -9999
+#numpy.savetxt(output_folder + 'python_slope_test.txt', slope_test)
+#
+#d = numpy.where(numpy.isnan(slope_test2))
+#slope_test2[c] = -9999
+#numpy.savetxt(output_folder + 'python_slope_test2.txt', slope_test2)
+"""
+
 # Open slope
 gSlope = gdal.Open(inputSlope, GA_ReadOnly)
 print 'Slope loaded sucessfully, checking file type...'
@@ -91,17 +129,45 @@ http://nbviewer.ipython.org/github/demotu/BMC/blob/master/notebooks/DetectPeaks.
 
 ## Calculate hypsometry (elevation over area)
 
-### Plot outputs
+""" # Plot hypsometry ??? """
+
+# Skewness test
+
+""" skewness threshold? """
+
+# Bimodal test
+
+""" bimodal threshold """
+
+### Plot inputs
 # Plot DEM
-DEMplot = plt.imshow(DEM, cmap='bone')
-plt.savefig(output_folder + 'DEM_plot.png')
-print 'DEM plotted successfully -- ' + output_folder + 'DEM_plot.png'
+fig_DEM = plt.figure(1)
+fig_DEM.suptitle('Greenland bedrock GIA DEM', fontsize=12)
+plt.imshow(DEM, cmap='terrain', vmax=2000, vmin=-800) # vmax is a way of stretching colour map
+
+plt.xlabel('Distance (km)', fontsize=10)
+plt.ylabel('Distance (km)', fontsize=10)
+cbar=plt.colorbar(extend='both')
+cbar.set_label('Elevation (m)', fontsize=10)
+plt.savefig(output_folder + 'DEM_plot.eps', dpi=1200)
+
+print 'DEM plotted successfully -- ' + output_folder + 'DEM_plot.eps'
 
 # Plot slope
-Splot = plt.imshow(slope, cmap='bone')
-plt.savefig(output_folder + 'Slope_plot.png')
-print 'Slope plotted successfully -- ' + output_folder + 'Slope_plot.png'
+fig_slope = plt.figure(2)
+fig_slope.suptitle('Slope of Greenland bedrock (GIA) DEM', fontsize=12)
+plt.imshow(slope, cmap='jet_r') # _r is a way to reverse colour map
 
+plt.xlabel('Distance (km)', fontsize=10)
+plt.ylabel('Distance (km)', fontsize=10)
+cbar=plt.colorbar(extend='neither')
+cbar.set_label('slope', fontsize=10)
+plt.savefig(output_folder + 'slope_plot.eps', dpi=1200)
+
+print 'Slope plotted successfully -- ' + output_folder + 'slope_plot.eps'
+
+### Plot outputs
+# Plot overall grid
 
 
 #X,Y = numpy.meshgrid(2501,3001)
