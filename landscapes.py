5c7cf6078b50e0e4de36525dea3f6865d44981b7
macGrIS <m.a.cooper@bristol.ac.uk>
2015-04-14 14:31:47 +0100

    landscape classifier working -- needs tidies

--- landscapes.py
+++ landscapes.py
@@ -44,21 +44,23 @@ inputSlope = input_folder + 'GDAL_slope2.tif' # must be a way to calculate this 
 
 ### Define parameters and metrics
 ## size of grid/ fishnet (in km -- for 1km posting data) -- LOOK INTO FUZZY BOXES LATER
-factor = 25 # doesn't seem to work for 150, or 200km grid sizes (why?)
+factor = 250 # doesn't seem to work for 150, or 200km grid sizes (why??)
 print 'Grid cell size = ' + str(factor) + ' km.' # units depend on pixel 'size'
 
 ## Input subsets (specific catchments?) - "do you wish to subset the data?" (perhaps at end...?)
 #local_factor = 50 # factor of subset
 
-## Decision Tree (based upon Jamieson, et al. 2014) -- Change for 'sensitivity analysis' -- ask for custom, or standard.
+# need to set up geolocation/ geotransform
+
+# Decision Tree (based upon Jamieson, et al. 2014) -- Change for 'sensitivity analysis' -- ask for custom, or standard.
 # Elevation range threshold lower
-#tree_elev_lower_thres = 1000 # orig < 1000 m
+tree_elev_lower_thres = 1000. # orig < 1000 m
 # Elevation range threshold upper
+tree_elev_upper_thres = 2000. # orig >= 1000 and < 2000 m
 # Peak density threshold
-#tree_elev_upper_thres = 2000 # orig >= 1000 and < 2000 m
-#tree_pd_thres = 20 # orig <= 25
+tree_pd_thres = 20. # orig <= 20
 # Slope threshold
-#tree_slope_thres = 45 # orig <= 5
+tree_slope_thres = 5. # orig <= 5
 
 ### Call input DEM
 # Open DEM
@@ -82,7 +84,7 @@ print 'Null values set.'
 
 ## Set up blank grid based upon factor
 peaky, peakx = DEM[0:3000,0:2500].shape
-p = numpy.zeros([peaky/factor,peakx/factor]) # new grid of factor size size -- to fill with peak density data
+p = numpy.zeros([peaky/factor,peakx/factor]) # new grid of factor size size -- to fill with metric data
 
 ### Calculate metrics
 ## Calculate slope -- need to add in this (possibly solved through convolution)
@@ -111,7 +113,7 @@ for i in numpy.nditer(slope_test, op_flags=['readwrite']):
 #slope_test2[c] = -9999
 #numpy.savetxt(output_folder + 'python_slope_test2.txt', slope_test2)
 """
-
+print 'Done.'
 # Open slope
 gSlope = gdal.Open(inputSlope, GA_ReadOnly)
 print 'Slope loaded sucessfully, checking file type...'
@@ -130,7 +132,7 @@ b = numpy.where(slope == sNull) # finds all values in array equal to nodata valu
 slope[b] = numpy.NaN # set nodata value to NaN
 print 'Null values set.'
 
-## Calculate peak analysis
+## Peak analysis
 # Open peak detection results from Landserf -- value of 5 depicts summit (to use for peak density)
 # Currently only using peaks1000 (as it displays ALL peaks over 1000 m in elevation) -- may require others later
 input_peaks1000 = input_folder + 'peak_anal/peaks_1000.tif'
@@ -161,7 +163,7 @@ peaks1000[peaks1000 == 5.] = 1.
 #peaks3000[numpy.logical_or(peaks3000 > 5., peaks3000 < 5.)] = 0.
 #peaks3000[peaks3000 == 5.] = 1.
 
-# Identify peaks - broken methodology -- maybe try local maximums?
+## Identify peaks - broken methodology -- maybe try local maximums?
 """
 need to open DEM, and identify high points/ peaks within array of 1000, 1500,
 2000, 2500 and 3000 metres with a minimum drop surrounding peak as 250 m
@@ -198,10 +200,10 @@ for j in range(1,DEM.shape[1] - 1):
 print 'Done.'
 """
 
-# Peak density
+## Calculate peak density
 print 'Calculating peak density per grid cell...'
 all_peaks_subset = peaks1000[0:3000, 0:2500]
-peak_dens_max, peak_dens_min, peak_dens_range, peak_dens_sum, regions = grid_range(all_peaks_subset, factor)
+peak_dens_max, peak_dens_min, peak_dens_range, peak_density, regions = grid_range(all_peaks_subset, factor)
 print 'Done.'
 
 ## Calculate elevation range
@@ -211,25 +213,12 @@ elev_max, elev_min, elev_range, elev_sum, regions = grid_range(DEM_subset, facto
 # numpy.savetxt(output_folder + 'elev_range.txt', elev_range)
 print 'Done.'
 
-# Elevation test for decision tree
-#elev_test = elev_range
-#elev_test[elev_test>elev_thres_1] = 1 ## may not work -- as two levels?
-#
-#if elev_test[elev_test>lower]
-
 ## Calculate slope range
 slope_subset = slope[0:3000,0:2500]
 slope_max, slope_min, slope_range, slope_sum, regions = grid_range(slope_subset, factor)
 print 'Calculating slope range per grid cell...'
 # numpy.savetxt(output_folder + 'slope_range.txt', slope_range)
 print 'Done.'
-
-# Slope test for decision tree
-#slope_test = slope_range
-# need an if loop otherwise will overwrite... 
-#slope_test[slope_range>tree_slope_thres] = 1
-
-"""  Do I need to classify these... put into binary, then grid (based upon thresholds above)? """
 
 ## Calculate hypsometry (elevation over area) -- chuck this into a defined function?
 # Monumentally inefficient loop (probably...)
@@ -253,7 +242,7 @@ for i in range(0,int(last_box)): # 0,last_box
         print 'Too many NaN values for cell: ' + str(i) + ', skipping...'
         continue
     else:
-        #fig_hist = plt.figure(i)
+        #fig_hist = plt.figure(1) # have this as plt.figure(i)? then below as i+1 etc.
         n, bins, patches = plt.hist(hypso_d[~numpy.isnan(hypso_d)], bins=100) # Histo plot, get rid of remaining NaNs
         #plt.show() # can output plots, but slow...!!
         
@@ -298,20 +287,18 @@ bimodal_test[bimodal_test>0]=1 # make bimodal_test binary
 bimodal_grid=bimodal_test.reshape(p.shape) # need to automate reshape -- perhaps elev_range.shape? (but if thats been done wrong then, could go wrong later)
 skewness_grid = skewness_test.reshape(p.shape)
 
-### Decision Tree
+### Decision Tree -- have to nest logical_and commands as they do not accept more than 2 arguments.
+landscape_gd = numpy.zeros(p.shape)
+# Areal Scour
+landscape_gd[elev_range < tree_elev_lower_thres] = 1.
+landscape_gd[numpy.logical_and(numpy.logical_and(elev_range >= tree_elev_lower_thres, elev_range < tree_elev_upper_thres), numpy.logical_and(bimodal_grid == 0., slope_range > tree_slope_thres))] = 1.
+# Selective Linear Erosion
+landscape_gd[numpy.logical_and(numpy.logical_and(elev_range >= tree_elev_lower_thres, elev_range < tree_elev_upper_thres), bimodal_grid == 1.)] = 2.
+landscape_gd[numpy.logical_and(elev_range >= tree_elev_upper_thres, skewness_grid == 1.)] = 2.
+landscape_gd[numpy.logical_and(numpy.logical_and(elev_range >= tree_elev_upper_thres, skewness_grid == 0.), peak_density <= tree_pd_thres)] = 2.
+# Mainly Alpine
+landscape_gd[numpy.logical_and(numpy.logical_and(elev_range >= tree_elev_upper_thres, skewness_grid == 0.), peak_density > tree_pd_thres)] = 3.
 
-"""
-is cell in bimodal_grid == 1:
-    
-    else 
-    
-    
-http://nbviewer.ipython.org/github/gumption/Python_for_Data_Science/blob/master/4_Python_Simple_Decision_Tree.ipynb
-"""
-
-# Use ravelled lists...?
-
-# if 
 
 ### Plotting
 ## Inputs
@@ -339,8 +326,8 @@ print 'Slope plotted successfully -- ' + output_folder + 'slope_plot.eps'
 
 ## Outputs
 # Identified Peaks
-fig_elev_range = plt.figure(4)
-fig_elev_range.suptitle('identified_peaks', fontsize=12)
+fig_id_peaks = plt.figure(4)
+fig_id_peaks.suptitle('identified_peaks', fontsize=12)
 plt.imshow(peaks1000, interpolation='nearest', cmap='Greys', extent=[0,2500,0,3000]) # interpolation 'nearest' stops blurry figures!
 plt.xlabel('Distance (km)', fontsize=10)
 plt.ylabel('Distance (km)', fontsize=10)
@@ -349,9 +336,9 @@ plt.savefig(output_folder + 'DEM_peaks_plot' + str(factor) + '.eps', dpi=1200)
 print 'Identified peaks plotted successfully -- ' + output_folder + 'DEM_peaks_plot.eps'
 
 # Peak Density
-fig_elev_range = plt.figure(5)
-fig_elev_range.suptitle('peak_density', fontsize=12)
-plt.imshow(peak_dens_sum, interpolation='nearest', extent=[0,2500,0,3000]) # interpolation 'nearest' stops blurry figures!
+fig_peak_density = plt.figure(5)
+fig_peak_density.suptitle('peak_density', fontsize=12)
+plt.imshow(peak_density, interpolation='nearest', extent=[0,2500,0,3000]) # interpolation 'nearest' stops blurry figures!
 plt.xlabel('Distance (km)', fontsize=10)
 plt.ylabel('Distance (km)', fontsize=10)
 cbar=plt.colorbar(extend='neither')
@@ -406,15 +393,13 @@ cbar=plt.colorbar(extend='neither')
 plt.savefig(output_folder + 'DEM_bimodal_test' + str(factor) + '.eps', dpi=1200)
 print 'Bimodal mask plotted successfully -- ' + output_folder + 'DEM_bimodal_test.eps'
 
+## Result
 # Plot Classified Grid
-#fig_classified
-
-# need to set up geolocation/ geotransform
-
-# - create a fishnet of variable size (as in set the size as a variable -- look into fuzzy boxes later)
-# - produce a result of elevation range, and slope range (as per fishnet)
-# - produce a fishnet with smaller arrays for elevation values per fishnet box
-# - produce histograms from fishnet (hypsometry)
-# -	work out skewness, and also smooth to work out bimodal??
-
-# plot results!
+fig_classified = plt.figure(10)
+fig_classified.suptitle('landscapes', fontsize=12)
+plt.imshow(landscape_gd, interpolation='nearest', extent=[0,2500,0,3000])
+plt.xlabel('Distance (km)', fontsize=10)
+plt.ylabel('Distance (km)', fontsize=10)
+cbar=plt.colorbar(extend='neither')
+plt.savefig(output_folder + 'DEM_landscapes_' + str(factor) + 'km.eps', dpi=1200)
+print 'Classified landscapes plotted successfully -- ' + output_folder + 'DEM_landscapes.eps'
